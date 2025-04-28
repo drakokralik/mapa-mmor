55a347b2eda98f286bdaaf3561a66bbfaad00daf
JoZi <107387653+LostJoZi@users.noreply.github.com>
2025-04-28 19:10:11 +0200

    Aktivace Mirroru - funguje

--- Assets/camera_movement.cs
+++ Assets/camera_movement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class camera_move : MonoBehaviour
 {
-
     public float CameraSensitivity = 10.0f;
     public float LookUpLimit = 70.0f;
 
@@ -13,15 +12,28 @@ public class camera_move : MonoBehaviour
 
     private float xRot = 0.0f;
     private float yRot = 0.0f;
-    // Start is called before the first frame update
+
     void Start()
     {
-
+        // Automaticky najde PlayerBody pokud není nastaven
+        if (PlayerBody == null)
+        {
+            if (transform.root != null)
+            {
+                PlayerBody = transform.root;
+            }
+            else
+            {
+                Debug.LogError("PlayerBody is not assigned and could not be found!");
+            }
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (PlayerBody == null)
+            return; // bezpeènostní pojistka, kdyby náhodou
+
         float mouseX = Input.GetAxis("Mouse X") * CameraSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * CameraSensitivity * Time.deltaTime;
 
