6f514783e28c9d5f6a14467cf516531a07bf5b3d
krylj <krystofhorna2007@gmail.com>
2025-05-31 18:44:02 +0200

    Revert "..."
    This reverts commit 1b505aae3666c4aae428ca1b7522072eb7433658.

--- Assets/Mirror/Examples/_Common/Controllers/TankController/TankHealth.cs
+++ Assets/Mirror/Examples/_Common/Controllers/TankController/TankHealth.cs
@@ -1,57 +1,83 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
 
-public class TankHealth : NetworkBehaviour
+namespace Mirror.Examples.Common.Controllers.Tank
 {
-    [SyncVar]
-    public int currentHP = 100;
-
-    public int maxHP = 100;
-    public byte respawnTime = 3;
-
-    private bool isRespawning = false;
-
-    private void Update()
+    [AddComponentMenu("")]
+    [DisallowMultipleComponent]
+    public class TankHealth : NetworkBehaviour
     {
-        if (!isServer) return; // logiku HP a respawnu dìlej jen na serveru
+        [Header("Components")]
+        public TextMesh healthBar;
 
-        if (currentHP <= 0 && !isRespawning)
+        [Header("Stats")]
+        public byte maxHealth = 5;
+        [SyncVar(hook = nameof(OnHealthChanged))]
+        public byte health = 5;
+
+        [Header("Respawn")]
+        public bool respawn = true;
+        public byte respawnTime = 3;
+
+        void OnHealthChanged(byte oldHealth, byte newHealth)
         {
-            StartCoroutine(RespawnCoroutine());
+            healthBar.text = new string('-', newHealth);
+
+            if (newHealth >= maxHealth)
+                healthBar.color = Color.green;
+            if (newHealth < 4)
+                healthBar.color = Color.yellow;
+            if (newHealth < 2)
+                healthBar.color = Color.red;
+            if (newHealth < 1)
+                healthBar.color = Color.black;
         }
-    }
 
-    [Server]
-    public void TakeDamage(int damage)
-    {
-        if (currentHP <= 0) return;
+        #region Unity Callbacks
 
-        currentHP -= damage;
-        if (currentHP < 0)
-            currentHP = 0;
-    }
+        protected override void OnValidate()
+        {
+            // Skip if Editor is in Play mode
+            if (Application.isPlaying) return;
 
-    [Server]
-    IEnumerator RespawnCoroutine()
-    {
-        isRespawning = true;
+            base.OnValidate();
+            Reset();
+        }
 
-        // Poèkej respawnTime sekund
-        yield return new WaitForSeconds(respawnTime);
+        public void Reset()
+        {
+            if (healthBar == null)
+                healthBar = transform.Find("HealthBar").GetComponent<TextMesh>();
+        }
 
-        // Najdi spawn pozici (pokud nemáš, respawn na Y=5)
-        Vector3 spawnPos = Vector3.up * 5f;
-        Transform spawnPoint = NetworkManager.singleton.GetStartPosition();
-        if (spawnPoint != null)
-            spawnPos = spawnPoint.position;
+        #endregion
 
-        // Pøesuò tank (hráèe) na spawn
-        transform.position = spawnPos;
+        public override void OnStartServer()
+        {
+            health = maxHealth;
+        }
 
-        // Resetuj HP
-        currentHP = maxHP;
+        [ServerCallback]
+        public void TakeDamage(byte damage)
+        {
+            // Skip if health is already 0
+            if (health == 0) return;
 
-        isRespawning = false;
+            if (damage > health)
+                health = 0;
+            else
+                health -= damage;
+
+            if (health == 0)
+            {
+                if (connectionToClient != null)
+                    Respawn.RespawnPlayer(respawn, respawnTime, connectionToClient);
+                else if (netIdentity.sceneId != 0)
+                    NetworkServer.UnSpawn(gameObject);
+                else
+                    Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Mirror/Examples/_Common/Scripts/Respawn.cs
+++ Assets/Mirror/Examples/_Common/Scripts/Respawn.cs
@@ -1,31 +1,39 @@
 using System.Collections;
 using UnityEngine;
-using Mirror;
 
 namespace Mirror.Examples.Common
 {
-    public static class Respawn
+    public class Respawn
     {
         public static void RespawnPlayer(bool respawn, byte respawnTime, NetworkConnectionToClient conn)
         {
+            // Use the NetworkManager static singleton to start a coroutine
             NetworkManager.singleton.StartCoroutine(DoRespawn(respawn, respawnTime, conn));
         }
 
-        static IEnumerator DoRespawn(bool respawn, byte respawnTime, NetworkConnectionToClient conn)
+        public static IEnumerator DoRespawn(bool respawn, byte respawnTime, NetworkConnectionToClient conn)
         {
+            //Debug.Log("DoRespawn started");
+
+            // Wait for SyncVars to Update
             yield return null;
 
+            // Remove Player
             if (!respawn)
             {
                 NetworkServer.RemovePlayerForConnection(conn, RemovePlayerOptions.Destroy);
+                //Debug.Log("Player destroyed");
                 yield break;
             }
 
             GameObject playerObject = conn.identity.gameObject;
             NetworkServer.RemovePlayerForConnection(conn, RemovePlayerOptions.Unspawn);
+            //Debug.Log("Player unspawned");
 
+            // Wait for respawn Time
             yield return new WaitForSeconds(respawnTime);
 
+            // Respawn Player - fallback to Vector3.up * 5f to avoid spawning on another player.
             Transform spawnPoint = NetworkManager.singleton.GetStartPosition();
             Vector3 position = spawnPoint != null ? spawnPoint.position : Vector3.up * 5f;
             Quaternion rotation = spawnPoint != null ? spawnPoint.rotation : Quaternion.identity;
@@ -33,11 +41,7 @@ namespace Mirror.Examples.Common
             playerObject.transform.SetPositionAndRotation(position, rotation);
 
             NetworkServer.AddPlayerForConnection(conn, playerObject);
-
-            if (playerObject.TryGetComponent(out PlayerCombat pc))
-            {
-                pc.ResetStats();
-            }
+            //Debug.Log("Player respawned");
         }
     }
 }
