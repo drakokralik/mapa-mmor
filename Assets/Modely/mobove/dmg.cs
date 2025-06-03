d4458609f6beb26a1acb8153c57cc6c03320c215
krylj <krystofhorna2007@gmail.com>
2025-06-03 20:37:06 +0200

    menší oprava

--- Assets/Modely/mobove/dmg.cs
+++ Assets/Modely/mobove/dmg.cs
@@ -7,12 +7,24 @@ public class MobAttack : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Mob collided with: " + collision.gameObject.name);
+
         if (collision.gameObject.CompareTag("Player"))
         {
             PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth == null)
+            {
+                playerHealth = collision.gameObject.GetComponentInParent<PlayerHealth>();
+            }
+
             if (playerHealth != null)
             {
                 playerHealth.TakeDamage(10);
+                Debug.Log("Player took 10 damage");
+            }
+            else
+            {
+                Debug.LogWarning("PlayerHealth script not found on player!");
             }
 
             Vector3 knockbackDirection = (transform.position - collision.transform.position).normalized;
